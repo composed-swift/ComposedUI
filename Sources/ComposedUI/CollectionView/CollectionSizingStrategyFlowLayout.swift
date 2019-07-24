1b75ab294f0c0fcc6efaa9c389b68d301306a933
Shaps Benkau <shapsuk@me.com>
2019-07-24 16:24:06 +0100

    Added column based sizing strategy for flow layouts

---
+++ Sources/ComposedUI/CollectionView/CollectionSizingStrategyFlowLayout.swift
@@ -0,0 +1,75 @@
+import UIKit
+
+public protocol CollectionSizingStrategyFlowLayout:CollectionSizingStrategy {
+    var metrics: CollectionSectionMetrics { get }
+}
+
+open class ColumnCollectionSizingStrategy: CollectionSizingStrategyFlowLayout {
+
+    public enum SizingMode {
+        case fixed(height: CGFloat)
+        case automatic(isUniform: Bool)
+        case aspect(ratio: CGFloat)
+    }
+
+    public let columnCount: Int
+    public let sizingMode: SizingMode
+    public let metrics: CollectionSectionMetrics
+
+    public init(columnCount: Int, sizingMode: SizingMode, metrics: CollectionSectionMetrics) {
+        self.columnCount = columnCount
+        self.sizingMode = sizingMode
+        self.metrics = metrics
+    }
+
+    private var cachedSizes: [Int: CGSize] = [:]
+    private func cachedSize(forElementAt index: Int) -> CGSize? {
+        switch sizingMode {
+        case .aspect:
+            return cachedSizes[index]
+        case .fixed:
+            return cachedSizes.values.first
+        case let .automatic(isUniform):
+            return isUniform ? cachedSizes.values.first : cachedSizes[index]
+        }
+    }
+
+    open func size(forElementAt index: Int, context: CollectionSizingContext) -> CGSize {
+        if let size = cachedSize(forElementAt: index) { return size }
+
+        var width: CGFloat {
+            let interitemSpacing = CGFloat(columnCount - 1) * metrics.minimumInteritemSpacing
+            let availableWidth = context.layoutSize.width - metrics.sectionInsets.left - metrics.sectionInsets.right - interitemSpacing
+            return (availableWidth / CGFloat(columnCount)).rounded(.down)
+        }
+
+        switch sizingMode {
+        case let .aspect(ratio):
+            let size = CGSize(width: width, height: width * ratio)
+            cachedSizes[index] = size
+            return size
+        case let .fixed(height):
+            let size = CGSize(width: width, height: height)
+            cachedSizes[index] = size
+            return size
+        case .automatic:
+            let targetView: UIView
+            let targetSize = CGSize(width: width, height: 0)
+
+            if let cell = context.prototype as? UICollectionViewCell {
+                targetView = cell.contentView
+            } else {
+                targetView = context.prototype
+            }
+
+            let size = targetView.systemLayoutSizeFitting(
+                targetSize,
+                withHorizontalFittingPriority: .required,
+                verticalFittingPriority: .fittingSizeLevel)
+
+            cachedSizes[index] = size
+            return size
+        }
+    }
+
+}
